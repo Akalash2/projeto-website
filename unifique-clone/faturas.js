83d15cee26a16fb866618bac9cda907a2127d8bb
Akalash2 <akalash939@gmail.com>
2022-11-16 15:59:17 -0300

    ok

--- unifique-clone/faturas.js
+++ unifique-clone/faturas.js
@@ -15,7 +15,7 @@ function atualizaFaturas(status) {
     // Atualiza os status dos botoes
     atualizaBotaoStatus(status);
 
-    if(isLoadDadosFixos()){
+    if (isLoadDadosFixos()) {
         // Carrega uma lista de dados fixos, para nao gastar todas as chamadas api
         var dadosFaturas = JSON.parse(LISTA_FATURAS);
         loadFaturas(dadosFaturas);
@@ -74,7 +74,7 @@ function loadFaturas(data) {
     }
 }
 
-function getHtmlBotaoBoletos(idFatura){
+function getHtmlBotaoBoletos(idFatura) {
     return `
      <a title="Cobrancas pagas" href="#" class="open-detalheFatura title" data-toggle="modal" data-id="${idFatura}" data-target="#modalFatura">
         <p class="title">Detalhar</p>
@@ -90,14 +90,61 @@ $(document).on("click", ".open-detalheFatura", function() {
 
     // Seta o id da fatura no modal
     $(".modal-body #idfatura").val(idFatura);
+
+    // chama a api para carregar os detalhes da fatura
+    loadDadosFatura(idFatura);
 });
 
-function loadDadosFatura(idFatura){
-    // criar a tabela de detalhes da fatura
+function loadDadosFatura(idFatura) {
+    // criar a tabela de detalhes da fatura - ok
 
     // Chamar a api do supabase
+    // Chama a api da Supabase
+    data = {};
+    port = "tbfaturadetalhe?idfatura=eq." + idFatura + "&";
+    callApi("GET", port, undefined, function(data) {
+        // Setar os dados da fatura no modal
+        loadDadosFaturaDetalhe(data);
+    });
+}
 
-    // criar a tabela de detalhes da fatura
+function loadDadosFaturaDetalhe(data) {
+    const faturasDetalhe = data;
+    debugger;
 
-    // Setar os dados da fatura no modal
+    let bodyTable = document.querySelector(".containerTableDetalhe-body");
+    // reset da tabela
+    bodyTable.innerHTML = '';
+
+    let temDados = false;
+    if (parseInt(faturasDetalhe.length) > 0) {
+        temDados = true;
+    }
+
+    if (temDados) {
+        faturasDetalhe.forEach(function(oFaturaDetalhe, key) {
+            const id = oFaturaDetalhe.id;
+            const idfaturaApi = oFaturaDetalhe.idfatura;
+            const circuito = oFaturaDetalhe.circuito;
+            const codigoservico = oFaturaDetalhe.codigoservico;
+            const descricaoservico = oFaturaDetalhe.descricaoservico;
+            const datainicioservico = oFaturaDetalhe.datainicioservico;
+            const datafimservico = oFaturaDetalhe.datafimservico;
+            const valorservico = oFaturaDetalhe.valorservico;
+
+            // colocando os detalhes da faturas na tabela de html
+            bodyTable.innerHTML += `<tr>
+                                            <td>${descricaoservico}</td>
+                                            <td>${datainicioservico}</td>
+                                            <td>${datafimservico}</td>
+                                            <td>${circuito}</td>
+                                            <td>${valorservico}</td>
+                                        </tr>`;
+
+        });
+    } else {
+        bodyTable.innerHTML = `<tr>
+                                    <td colspan="5" style="color:red;font-size:25px; text-align:center;">Fatura sem Detalhes!</td>
+                                </tr>`;
+    }
 }
--- unifique-clone/token.js
+++ unifique-clone/token.js
@@ -1,7 +1,8 @@
 function getUrlBase(port) {
-    return "https://vdcszqvvrwdqcnjvcoxt.supabase.co/rest/v1/countries?select=id" + port;
+    //return "https://vdcszqvvrwdqcnjvcoxt.supabase.co/rest/v1/countries?select=id" + port;
+    return "https://leksjtsdzqyveugkqrtm.supabase.co/rest/v1/countries?select=id" + port;
 }
 
 function getTokenSupabase() {
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxla3NqdHNkenF5dmV1Z2txcnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzNzYsImV4cCI6MTk3ODU3NzM3Nn0.6Y2bYbx4cNNbMONWxm-Lr6fFj7T7VlK4z6CMnmaRcOk";
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxla3NqdHNkenF5dmV1Z2txcnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzNzYsImV4cCI6MTk3ODU3NzM3Nn0.6Y2bYbx4cNNbMONWxm-Lr6fFj7T7VlK4z6CMnmaRcOk.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxla3NqdHNkenF5dmV1Z2txcnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzNzYsImV4cCI6MTk3ODU3NzM3Nn0.6Y2bYbx4cNNbMONWxm-Lr6fFj7T7VlK4z6CMnmaRcOk";
 }
